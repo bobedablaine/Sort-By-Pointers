f2eab5fff79d5545f7cb152a3b35919f14683dac
jacobberry <70248350+bobedablaine@users.noreply.github.com>
2023-10-04 21:55:14 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 
 # Author: Jacob Berry
-# Email: jberry19@csu.fullerton.edu
+# Email: jacobberry777@gmail.com
 # Class: CPSC 240-03
 # Assignment 3: Sort By Pointers
 
 rm *.o
 rm *.out
 
-echo "Bash script for <Array Management System>"
+echo "Bash script for <Sort By Pointers>"
 
 echo "Assemble the modules written in ASM"
 nasm -f elf64 -l director.lis -o director.o director.asm
@@ -23,7 +23,7 @@ gcc -c -Wall -o output_array.o -m64 -no-pie -fno-pie output_array.c
 echo "Link the object files"
 g++ -m64 main.o director.o input_array.o output_array.o sort_pointers.o -fno-pie -no-pie -o main.out
 
-echo "Run the program Array Management System"
+echo "Run the program Sort By Pointers"
 ./main.out
 
 echo "The bash script file is now closing."
